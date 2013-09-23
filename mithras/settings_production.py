58fe6a0fcff05eb90f35a1b8b2fa68de269ed972
Anders Pearson <anders@columbia.edu>
2013-09-22 21:54:29 -0400

    logging bugfix for 1.5

--- mithras/settings_production.py
+++ mithras/settings_production.py
@@ -9,6 +9,11 @@ TEMPLATE_DIRS = (
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+}
+
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
 try:
