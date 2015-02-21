8f58cd9ec133ef1618ea1859699efe76a7971752
Anders Pearson <anders@columbia.edu>
2015-02-21 13:59:57 +0100

    sorl is not used anymore, so take it out

--- mithras/settings_shared.py
+++ mithras/settings_shared.py
@@ -120,7 +120,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mithras.abraxas',
-    'sorl.thumbnail',
     'django_nose',
     'django.contrib.sitemaps',
     'django_statsd',
