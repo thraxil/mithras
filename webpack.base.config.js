b3836d65285190c04498d2dfebe5e537ece6d28d
Anders Pearson <anders@columbia.edu>
2015-12-10 19:47:07 +0000

    split out masthead css to ensure correct image path

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -28,6 +28,7 @@ module.exports = {
             { test: /\.ejs$/, loader: "ejs-loader" },
             { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
             { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.jpg$/, loader: "file-loader?name=[path][name].[ext]" },
             { test: /\.less$/, loader: "style!css!less" },
             { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
             { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
