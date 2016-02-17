1a347455153d8f21020d8db4130678dee41edda6
Anders Pearson <anders@columbia.edu>
2016-02-17 14:56:20 +0000

    non empty root integration test
    should hit a lot of coverage

--- mithras/abraxas/tests/test_integration.py
+++ mithras/abraxas/tests/test_integration.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test.client import Client
+from .factories import PostFactory
 
 
 class BasicTest(TestCase):
@@ -9,6 +10,12 @@ class BasicTest(TestCase):
     def test_empty_root(self):
         response = self.c.get("/")
         self.assertEqual(response.status_code, 200)
+
+    def test_nonempty_root(self):
+        p = PostFactory()
+        response = self.c.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(p.node.title in response.content)
 
     def test_smoketest(self):
         response = self.c.get("/smoketest/")
