6dc5707febfe4462a46a685f45af6ecfcc3cbc91
Anders Pearson <anders@thraxil.org>
2017-10-18 21:00:56 +0100

    smoketest to check migrations

---
+++ mithras/abraxas/check_migrations.py
@@ -0,0 +1,25 @@
+from django.db import DEFAULT_DB_ALIAS, connections
+from django.db.migrations.loader import MigrationLoader
+""" idea from this article:
+
+http://tech.octopus.energy/news/2016/05/05/django-elb-health-checks.html
+
+"""
+
+
+def migrations_have_applied():
+    """
+    Check if there are any migrations that haven't been applied yet
+    """
+    connection = connections[DEFAULT_DB_ALIAS]
+    loader = MigrationLoader(connection)
+    graph = loader.graph
+
+    # Count unapplied migrations
+    num_unapplied_migrations = 0
+    for app_name in loader.migrated_apps:
+        for node in graph.leaf_nodes(app_name):
+            for plan_node in graph.forwards_plan(node):
+                if plan_node not in loader.applied_migrations:
+                    num_unapplied_migrations += 1
+    return num_unapplied_migrations == 0
--- mithras/abraxas/smoke.py
+++ mithras/abraxas/smoke.py
@@ -1,5 +1,6 @@
 from smoketest import SmokeTest
 from django.contrib.auth.models import User
+from .check_migrations import migrations_have_applied
 
 
 class DBConnectivity(SmokeTest):
@@ -7,3 +8,6 @@ class DBConnectivity(SmokeTest):
         cnt = User.objects.all().count()
         # all we care about is not getting an exception
         self.assertTrue(cnt > -1)
+
+    def test_migrations(self):
+        self.assertTrue(migrations_have_applied())
