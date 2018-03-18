751d10132039ab7f643f1393da3f41b3f7396533
Anders Pearson <anders@thraxil.org>
2018-03-18 20:41:36 +0000

    no git release

--- mithras/settings_docker.py
+++ mithras/settings_docker.py
@@ -23,7 +23,4 @@ RAVEN_DSN = os.environ.get('RAVEN_DSN', None)
 if RAVEN_DSN:
     RAVEN_CONFIG = {
         'dsn': RAVEN_DSN,
-        # If you are using git, you can also automatically configure the
-        # release based on the git info.
-        'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
     }
