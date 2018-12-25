83b1f9b2dc11e105243989f414a97a98f31356c2
Anders Pearson <anders@thraxil.org>
2018-12-25 11:31:02 +0000

    experimental opencensus integration

--- mithras/settings_shared.py
+++ mithras/settings_shared.py
@@ -1,13 +1,14 @@
 # Django settings for mithras project.
 import os.path
 from thraxilsettings.shared import common
+from opencensus.trace import config_integration
 
 app = 'mithras'
 base = os.path.dirname(__file__)
 
 locals().update(common(app=app, base=base))
 
-ALLOWED_HOSTS += ['.thraxil.org', 'thraxil.org']  # noqa
+ALLOWED_HOSTS += ['.thraxil.org', 'thraxil.org', '127.0.0.1']  # noqa
 
 MEDIA_ROOT = '/var/www/thraxil/uploads/'
 
@@ -38,11 +39,31 @@ INSTALLED_APPS += [  # noqa
     'expvarcmdline',
     'expvarpsutil',
     'bootstrap3',
+
+    'opencensus.trace.ext.django',
 ]
 
 MIDDLEWARE += [ # noqa
     'django.contrib.messages.middleware.MessageMiddleware',
+    'opencensus.trace.ext.django.middleware.OpencensusMiddleware',
 ]
+
+OPENCENSUS_TRACE = {
+    'SAMPLER': 'opencensus.trace.samplers.always_on.AlwaysOnSampler',
+    'EXPORTER': 'opencensus.trace.exporters.print_exporter.PrintExporter',
+    #    'PROPAGATOR':
+    #    'opencensus.trace.propagation.google_cloud_format.'
+    #    'GoogleCloudFormatPropagator',
+}
+
+OPENCENSUS_TRACE_PARAMS = {
+    'SAMPLING_RATE': 0.5,
+    'SERVICE_NAME': 'mithras',
+}
+
+integration = ['postgresql']
+
+config_integration.trace_integrations(integration)
 
 SERVER_EMAIL = 'moderation@thraxil.org'
 USE_TZ = False
